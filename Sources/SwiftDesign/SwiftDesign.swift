fca92036d147bf10adbd5e2b63451fda87fa0932
Goga Barabadze <goga.barabadze73@gmail.com>
2021-04-12 19:39:55 +0200

    Initial

--- Sources/SwiftDesign/SwiftDesign.swift
+++ Sources/SwiftDesign/SwiftDesign.swift
@@ -1,3 +1,21 @@
-struct SwiftDesign {
-    var text = "Hello, World!"
+import UIKit
+
+extension UIView {
+    
+    public func roundCorners(radius: CGFloat, masksToBounds: Bool = false) {
+        
+        self.layer.masksToBounds = masksToBounds
+        self.layer.cornerRadius = radius
+        
+    }
+    
+    public func addShadow(radius: CGFloat = 4.0, opacity: Float = 0.2, offset: CGSize = CGSize(width: 3, height: 3), color: UIColor = .black) {
+        
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOffset = offset
+        self.layer.shadowOpacity = opacity
+        self.layer.shadowRadius = radius
+        
+    }
+    
 }
